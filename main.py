e3697409ca3fd0a909c3bc2f8bcfac0460cddb5a
ksg-dev <ksg.dev.data@gmail.com>
2024-06-10 16:07:17 -0500

    gui outline comments

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window set up
+window = Tk()
+window.title("MyPass Password Manger")
+window.minsize(width=500, height=300)
+
+# Add logo
+
+
+# Label - website
+
+
+# Entry - website input
+
+
+# Label - email/username
+
+
+# Entry - email/username input
+
+
+# Label - password
+
+
+# Entry - password input
+
+
+# Buttons
+# Generate password
+
+
+# Add
+
+
+window.mainloop()
